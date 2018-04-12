ee04fbdf56ac834ab99504f448e03bac7a4f53cc
Chuck Lu <chuck.lu@qq.com>
2018-04-12 18:15:12 +0800

    refactor the script for git fetch

--- RecursiveGitFetch.bat
+++ RecursiveGitFetch.bat
@@ -2,12 +2,25 @@
 echo Mission Started: %date% %time%
 setlocal EnableDelayedExpansion
 set repositoryCount=0
-Call :GitFetch %cd%
+Call :RecursiveFolders %cd%
 echo Mission Completed: %date% %time%
 Exit /B 0
 
-:GitFetch
+:RecursiveFolders
 IF EXIST .git (
+Call :GitFetch
+)
+Call :CountSubFolders "%~1",value2
+if not !value2! == 0 (
+for /d %%i in ("%~1"\*) do (
+cd "%%~i"
+Call :RecursiveFolders "%%~i"
+cd ..
+) 
+)
+exit /b %ERRORLEVEL%
+
+:GitFetch
 set /a repositoryCount+=1
 echo !repositoryCount! %cd%
 git remote -v 
@@ -15,16 +28,8 @@ echo Fetch Started: %date% %time%
 git fetch --all
 echo Fetch Completed: %date% %time%
 echo.
-)
-Call :CountSubFolders "%*",value2
-if not !value2! == 0 (
-for /d %%i in ("%~1"\*) do (
-cd "%%~i"
-Call :GitFetch "%%~i"
-cd ..
-) 
-)
 exit /b %ERRORLEVEL%
+
 
 :CountSubFolders
 set "count=0"
