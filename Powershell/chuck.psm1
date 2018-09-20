2afb67b38f739ba75ba81cae78aa78473a75cd8b
Chuck Lu <chuck.lu@qq.com>
2018-09-20 18:53:31 +0800

    reset all items under specified folder with current time

---
+++ Powershell/chuck.psm1
@@ -0,0 +1,11 @@
+function Reset-FolderTime
+{
+Param(
+[parameter(Mandatory=$true)]
+[ValidateNotNullOrEmpty()]
+[String]
+$targetDir)
+Get-ChildItem -Path $targetDir -Recurse | Foreach-Object {
+    $_.LastWriteTime = Get-Date
+}
+}
