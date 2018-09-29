3eaddf4615c2acbd55f4e243d8672da2a0e8e757
Chuck Lu <chuck.lu@qq.com>
2018-09-29 17:52:21 +0800

    download the leela zero zip file and unpack it

--- Python/test.py
+++ Python/test.py
@@ -1,23 +1,24 @@
-#! /usr/bin/env python
-import urllib2
-import StringIO
-import gzip
+import os
+leeladir="leela-zero-0.15-win64"
+path=os.getcwd()
+print ("Current working dir : %s" % path)
+print ("list files in dir: %s" % path)
+for entry in os.scandir(path):
+        print(entry.name)
+    
+import shutil
+shutil.rmtree(leeladir)
+    
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
+# or: requests.get(url).content
 
-baseURL = "https://www.kernel.org/pub/linux/docs/man-pages/"
-filename = "man-pages-3.34.tar.gz"
-outFilePath = "man-pages-3.34.tar"
-
-response = urllib2.urlopen(baseURL + filename)
-compressedFile = StringIO.StringIO()
-compressedFile.write(response.read())
-#
-# Set the file's current position to the beginning
-# of the file so that gzip.GzipFile can read
-# its contents from the top.
-#
-compressedFile.seek(0)
-
-decompressedFile = gzip.GzipFile(fileobj=compressedFile, mode='rb')
-
-with open(outFilePath, 'w') as outfile:
-    outfile.write(decompressedFile.read())
+resp = urlopen("https://github.com/gcp/leela-zero/releases/download/v0.15/leela-zero-0.15-win64.zip")
+zipfile = ZipFile(BytesIO(resp.read()))
+file = zipfile.namelist()
+print(file)
+zipfile.extractall()
+zipfile.close()
+for entry in os.scandir(path):
+        print(entry.name)
