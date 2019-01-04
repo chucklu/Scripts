c410326a783821c4f23b2f2c20f12bdd978046ce
Chuck Lu <chuck.lu@qq.com>
2019-01-04 10:17:42 +0800

    output the sql file name to log

--- BulkSqlFiles.bat
+++ BulkSqlFiles.bat
@@ -1,2 +1,11 @@
-for %%G in (*.sql) do sqlcmd -S 127.0.0.1 -d datbaseName -U login -P "password" -i"%%G" -I >> sql.log
+@echo off
+for %%G in (*.sql) do (
+rem output file name of sql
+echo %%G >> sql.log
+rem execute the sql by sqlcmd
+sqlcmd -S 127.0.0.1 -d datbaseName -U login -P "password" -i "%%G" -I >> sql.log
+rem output two new line
+echo. >> sql.log
+echo. >> sql.log
+)
 pause
