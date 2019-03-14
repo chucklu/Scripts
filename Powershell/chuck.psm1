3f3f50057c3d4babe9b0e5908d429140d434f978
Chuck Lu <chuck.lu@qq.com>
2019-03-14 20:30:57 +0800

    integrate the convert encoding UTF8NoBOM into chuck module

--- Powershell/chuck.psm1
+++ Powershell/chuck.psm1
@@ -8,3 +8,20 @@ function Reset-FolderTime {
         $_.LastWriteTime = Get-Date
     }
 }
+
+function Convert-EncodingToUTF8NoBOM{
+    Param(
+        [parameter(Mandatory = $false)]
+        [String]
+        $targetDir = '.',
+        [parameter(Mandatory = $false)]
+        $extension = '*')
+        $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+        $files =  Get-ChildItem -Path $targetDir -Filter $extension
+        foreach($file in $files)
+        {
+          #$file.Name
+          $fileContent = Get-Content -Path $file.Name
+          [System.IO.File]::WriteAllLines($file.Name, $fileContent, $Utf8NoBomEncoding)
+        }
+}
