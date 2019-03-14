496f623dd20e6b49c0ae393c52e6da52ff341374
Chuck Lu <chuck.lu@qq.com>
2019-03-14 20:31:55 +0800

    add the $PROFILE to git C:\Users\clu\Documents\PowerShell\Microsoft.PowerShell_profile.ps1

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Import-Module 'C:\Users\clu\source\repos\GitHub\Other\posh-git\src\posh-git.psd1'
+Import-Module 'C:\Users\clu\source\repos\GitHub\Other\PowerShell\build.psm1'
+#Import-Module "C:\Users\clu\source\repos\GitHub\Other\PowerShell\src\Modules\Windows\Microsoft.PowerShell.Management\Microsoft.PowerShell.Management.psd1"
+Import-Module 'C:\Users\clu\source\repos\GitHub\ChuckLu\Scripts\Powershell\chuck.psm1'
+#Import-Module "C:\Users\clu\source\repos\GitHub\Other\PowerShellGet\PowerShellGet"
+#Import-Module "C:\Users\clu\source\repos\GitHub\Other\PowerShellGet\PowerShellGet\private\functions\Get-PSGalleryApiAvailability.ps1"
+Import-WinModule Microsoft.PowerShell.Management
