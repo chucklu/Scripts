fe14e2686a8961e64f9c08e8da889ae4603953bd
Chuck Lu <chuck.lu@qq.com>
2019-03-28 19:18:58 +0800

    rebuild multi solutions

--- RebuildSolutions.ps1
+++ RebuildSolutions.ps1
@@ -1,7 +1,28 @@
 $msBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community-Preview\Common7\IDE\devenv.com"
-$log = "build.log"
-$solutionName = $args[0]
-$mode = $args[1]
+
+$location = Get-Location
+
+$folder = "{0}\Log" -f $location
+$logFolderExists = Test-Path -Path $folder
+If(!$logFolderExists)
+{
+    New-Item -ItemType Directory -Force -Path $folder
+}
+#$folder
+
+$log = "build_{0}.log" -f (Get-Date).ToString("yyyyMMdd")
+$log = Join-Path -Path $folder -ChildPath $log
+$logFileExists = Test-Path -Path $log
+if(!$logFileExists)
+{
+    New-Item $log
+}
+#$log
+
+$mode = $args[0]
+$solutions = "solution1.sln","solution2.sln","solution3.sln"
 # "& $msBuild $solutionName /nologo -verbosity:normal /Rebuild $mode"
 # -verbosity:normal is ot supported in vs 2019
-& $msBuild $solutionName /nologo /Rebuild $mode >> $log 2>&1
+foreach ($solution in $solutions) {
+    & $msBuild $solution /nologo /Rebuild $mode 2>&1 >> $log
+}
