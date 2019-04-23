a38209daa38129340d0a2d87f4f9eb473d951d07
Chuck Lu <chuck.lu@qq.com>
2019-04-23 14:55:04 +0800

    get file content with default encoding

--- Powershell/chuck.psm1
+++ Powershell/chuck.psm1
@@ -16,12 +16,13 @@ function Convert-EncodingToUTF8NoBOM {
     $targetDir = '.',
     [Parameter(Mandatory = $false)]
     $extension = '*')
+  $DefaultEncoding = [System.Text.Encoding]::Default
   $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
   $files = Get-ChildItem -Path $targetDir -Filter $extension
   foreach ($file in $files)
   {
     #$file.FullName
-    $fileContent = Get-Content -Path $file.FullName
+    $fileContent = Get-Content -Encoding $DefaultEncoding -Path $file.FullName
     [System.IO.File]::WriteAllLines($file.FullName,$fileContent,$Utf8NoBomEncoding)
   }
 }
